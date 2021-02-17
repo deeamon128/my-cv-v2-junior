3f2c4e9278ec16950d1804c65270fceb68b0d9b2
paulgsm <58441846+paulgsm@users.noreply.github.com>
2021-02-17 09:08:01 +0000

    json done and happy

--- src/_component/about-me/argh.js
+++ src/_component/about-me/argh.js
@@ -3,11 +3,13 @@
 [
     "Paul", 0,
     "Cami", 1,
-    "Lucas", 2,
-
+    "Lucas", 2
+]
 //object collection
 {
-    name:Paul,
-    age:27,
-    location: Worcester,
+    name: "Paul",
+    age: 27,
+    location:"Worcester"
 }
+
+
