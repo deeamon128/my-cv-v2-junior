95f9c3619c2490d719e392cef2d4c6fe6e6b176c
Iosif <iosifscrepy@gmail.com>
2021-03-06 12:32:52 +0000

    installed env

--- gulp/pug.js
+++ gulp/pug.js
@@ -1,60 +1,65 @@
-'use strict';
+"use strict";
 
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import {
   getJsonData,
   printError,
   fixWindows10GulpPathIssue,
   printCompile,
-  logError
-} from './util/util';
+  logError,
+} from "./util/util";
 
-const pug = ({ gulp, taskTarget, config, plugins, args, browserSync, baseUrl }) => {
+const pug = ({
+  gulp,
+  taskTarget,
+  config,
+  plugins,
+  args,
+  browserSync,
+  baseUrl,
+}) => {
   const dir = config.directory;
   const dataPath = path.join(dir.source, dir.data);
-  const embedPath = path.join(taskTarget, 'embed.css');
+  const embedPath = path.join(taskTarget, "embed.css");
 
-  gulp.task('pug', () => {
+  gulp.task("pug", () => {
     printCompile(compileMode, args);
     let data = getJsonData({ dataPath }) || {},
       reload = true;
 
-    browserSync.sockets.emit('msg', {
+    browserSync.sockets.emit("msg", {
       title: `<div style='font-size: 3rem; text-align-center'>Rerefshing web page</div>`,
-      body: `<h1 style='color: black; font-size: 2rem'>as you've made changes <br>to your PUG file 💃</h1>`
+      body: `<h1 style='color: black; font-size: 2rem'>as you've made changes <br>to your PUG file 💃</h1>`,
     });
 
     return (
       gulp
         // target pug files
         .src([
-          path.join(dir.source, '**/*.pug'),
+          path.join(dir.source, "**/*.pug"),
           // Ignore files and folders that start with '_'
-          '!' + path.join(dir.source, '{**/_*,**/_*/**}')
+          "!" + path.join(dir.source, "{**/_*,**/_*/**}"),
         ])
         // .pipe(plugins.debug())
         // Only deal with files that change in the pipeline
         .pipe(
           plugins.if(
-            compileMode === 'current',
+            compileMode === "current",
             plugins.changedInPlace({ firstPass: true })
           )
         )
         // Render if any pug files is changed and compare
         // the output with the destination file
+        .pipe(plugins.if(compileMode === "all", plugins.changed(taskTarget)))
         .pipe(
-          plugins.if(
-            compileMode === 'all',
-            plugins.changed(taskTarget)
-          )
+          plugins.plumber({
+            errorHandler: plugins.notify.onError({
+              title: "Error converting PUG",
+              message: "Error: <%= error.message %>",
+            }),
+          })
         )
-        .pipe(plugins.plumber({
-          errorHandler: plugins.notify.onError({
-            title: 'Error converting PUG',
-            message: 'Error: <%= error.message %>'
-          })}
-        ))
         // compile pug to html
         .pipe(
           plugins.pug({
@@ -67,18 +72,18 @@ const pug = ({ gulp, taskTarget, config, plugins, args, browserSync, baseUrl }) 
               // debug: true,
               data,
               taskTarget,
-              embedPath
-            }
+              embedPath,
+            },
           })
         )
-        .pipe(plugins.notify({
-          title: 'Pug Starter - CodeTap',
-          message: 'Converting PUG into beautiful HTML'
-        }))
-        .on('error', function(error) {
+        // .pipe(plugins.notify({
+        //   title: 'Pug Starter - CodeTap',
+        //   message: 'Converting PUG into beautiful HTML'
+        // }))
+        .on("error", function (error) {
           browserSync.notify(printError(error), 25000);
           reload = false;
-          this.emit('end');
+          this.emit("end");
           logError(error.name, error.message);
         })
         // Check if embed.css exists and use inlineSource to inject it
@@ -86,20 +91,20 @@ const pug = ({ gulp, taskTarget, config, plugins, args, browserSync, baseUrl }) 
           plugins.if(
             fs.existsSync(embedPath),
             plugins.inlineSource({
-              rootpath: path.join(__dirname, '..'),
-              attribute: 'embed',
+              rootpath: path.join(__dirname, ".."),
+              attribute: "embed",
             })
           )
         )
         // Fix for Windows 10 and gulp acting crazy
         .pipe(
-          plugins.rename(file => {
+          plugins.rename((file) => {
             const dest = taskTarget;
             fixWindows10GulpPathIssue({ file, dest, plugins, config });
           })
         )
         .pipe(gulp.dest(path.join(taskTarget)))
-        .on('end', () => {
+        .on("end", () => {
           reload && browserSync.reload();
         })
     );
@@ -107,5 +112,3 @@ const pug = ({ gulp, taskTarget, config, plugins, args, browserSync, baseUrl }) 
 };
 
 export default pug;
-
-
